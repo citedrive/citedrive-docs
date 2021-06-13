b2d3d44a8f7448f639ed9b4cc06bdb2e1ded445d
Gareth Bilaney <gareth.bilaney@hhu.de>
2021-06-13 21:45:54 +0200

    ...

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/garethii/Documents/GitHub/citedrive-docs/src/css/custom.css"),
-];
